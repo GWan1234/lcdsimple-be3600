d9b3ce3f1e7276e96b81df374eec3cf2280afa3d
Jason <xiaobao@linkease.com>
2025-09-10 13:48:29 +0800

    remove demos

--- lcdsimple/src-glinet-be3600/lv_conf.h
+++ lcdsimple/src-glinet-be3600/lv_conf.h
@@ -1026,7 +1026,7 @@
 #define LV_USE_TFT_ESPI         0
 
 /*Driver for evdev input devices*/
-#define LV_USE_EVDEV    0
+#define LV_USE_EVDEV    1
 
 /*Driver for libinput input devices*/
 #define LV_USE_LIBINPUT    0
@@ -1080,22 +1080,22 @@
  ====================*/
 
 /*Show some widget. It might be required to increase `LV_MEM_SIZE` */
-#define LV_USE_DEMO_WIDGETS 1
+#define LV_USE_DEMO_WIDGETS 0
 
 /*Demonstrate the usage of encoder and keyboard*/
-#define LV_USE_DEMO_KEYPAD_AND_ENCODER 1
+#define LV_USE_DEMO_KEYPAD_AND_ENCODER 0
 
 /*Benchmark your system*/
-#define LV_USE_DEMO_BENCHMARK 1
+#define LV_USE_DEMO_BENCHMARK 0
 
 /*Render test for each primitives. Requires at least 480x272 display*/
-#define LV_USE_DEMO_RENDER 1
+#define LV_USE_DEMO_RENDER 0
 
 /*Stress test for LVGL*/
-#define LV_USE_DEMO_STRESS 1
+#define LV_USE_DEMO_STRESS 0
 
 /*Music player demo*/
-#define LV_USE_DEMO_MUSIC 1
+#define LV_USE_DEMO_MUSIC 0
 #if LV_USE_DEMO_MUSIC
     #define LV_DEMO_MUSIC_SQUARE    0
     #define LV_DEMO_MUSIC_LANDSCAPE 0
@@ -1105,16 +1105,16 @@
 #endif
 
 /*Flex layout demo*/
-#define LV_USE_DEMO_FLEX_LAYOUT     1
+#define LV_USE_DEMO_FLEX_LAYOUT     0
 
 /*Smart-phone like multi-language demo*/
-#define LV_USE_DEMO_MULTILANG       1
+#define LV_USE_DEMO_MULTILANG       0
 
 /*Widget transformation demo*/
-#define LV_USE_DEMO_TRANSFORM       1
+#define LV_USE_DEMO_TRANSFORM       0
 
 /*Demonstrate scroll settings*/
-#define LV_USE_DEMO_SCROLL          1
+#define LV_USE_DEMO_SCROLL          0
 
 /*Vector graphic demo*/
 #define LV_USE_DEMO_VECTOR_GRAPHIC  0
--- lcdsimple/src-glinet-be3600/main.c
+++ lcdsimple/src-glinet-be3600/main.c
@@ -64,53 +64,6 @@ static void lv_indev_init(void)
 #endif
 }
 
-void btn_event_cb(lv_event_t * e) {
-    lv_obj_t * label = lv_event_get_user_data(e);
-    lv_label_set_text(label, "Button Clicked!");
-}
-
-void lv_example_001(void)
-{
-    /* 创建一个基础对象作为屏幕 */
-    lv_obj_t *scr = lv_screen_active();
-    
-    /* 设置背景色为浅灰色 */
-    lv_obj_set_style_bg_color(scr, lv_color_hex(0xf0f0f0), 0);
-    
-    /* 创建一个标签 */
-    lv_obj_t *label = lv_label_create(scr);
-    lv_label_set_text(label, "Hello LVGL 9.2!");
-    
-    /* 居中标签 */
-    lv_obj_center(label);
-    
-    /* 设置标签样式 */
-    lv_obj_set_style_text_font(label, &lv_font_montserrat_24, 0);
-    lv_obj_set_style_text_color(label, lv_color_hex(0x000000), 0);
-    
-    /* 添加一个旋转测试标记 */
-    lv_obj_t *marker = lv_label_create(scr);
-    lv_label_set_text(marker, "↑ Top");
-    lv_obj_align(marker, LV_ALIGN_TOP_MID, 0, 10);
-    
-    /* 刷新显示 */
-    lv_refr_now(NULL);
-}
-
-void lv_example_002(void) { 
-    lv_obj_t * scr = lv_obj_create(NULL); 
-    lv_obj_set_size(scr, 284, 76);    
-    lv_obj_set_style_bg_color(scr, lv_color_hex(0x00FF00), LV_PART_MAIN);
-    lv_obj_set_style_bg_opa(scr, LV_OPA_COVER, LV_PART_MAIN); 
-    lv_obj_t * textarea = lv_textarea_create(scr);     
-    lv_obj_set_size(textarea, 250, 50); 
-    lv_obj_center(textarea); 
-    lv_textarea_set_text(textarea, "测试内容test_yes\n");    
-    lv_textarea_set_one_line(textarea, false); 
-    lv_textarea_set_cursor_click_pos(textarea, true); 
-    lv_scr_load(scr);
-}
-
 int main(void)
 {
     lv_init();
@@ -119,12 +72,6 @@ int main(void)
     lv_linux_disp_init();
     lv_indev_init();
 
-    /*Create a Demo*/
-    //lv_demo_widgets();
-    //lv_demo_widgets_start_slideshow();
-    //lv_demo_flex_layout();
-    //lv_example_001();
-    //lv_example_002();
     setup_ui(&guider_ui);
 
     /*Handle LVGL tasks*/
